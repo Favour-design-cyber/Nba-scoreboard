0bed0488f32557dcd393b158eeaf35778a2c9b95
Favour-design-cyber <oluwadamilarefavour2000@gmail.com>
2023-01-17 12:41:14 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const warriorsGames = [ {
+  awayTeam: {
+    team: 'Golden State',
+    points: 119,
+    isWinner:true
+  },
+  homeTeam: {
+    team: 'Houston',
+    points: 106,
+    isWinner: false
+  }
+},
+{
+  awayTeam: {
+    team: 'Golden State',
+    points: 105,
+    isWinner: false
+  },
+  homeTeam: {
+    team: 'Houston',
+    points:98,
+    isWinner:true
+  }
+},
+
+{
+  homeTeam: {
+    team: 'Golden State',
+    points: 115,
+    isWinner:true
+  },
+
+  awayTeam: {
+    team: 'Huston',
+    points: 86,
+    isWinner: false
+  }
+},
+
+{
+  awayTeam:{
+    team: 'Golden State',
+    points:101,
+    isWinner:true
+  },
+  homeTeam: {
+    team: 'Houston',
+    points: 92,
+    isWinner: false
+  }
+}
+
+]
